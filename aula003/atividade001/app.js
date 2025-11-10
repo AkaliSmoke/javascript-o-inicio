935f72e40cdaf36b4b004a609b8cbdf42d8f8fb4
Kaio <72668772+AkaliSmoke@users.noreply.github.com>
2025-11-10 20:42:53 -0300

    atividade001
    adição dos valores e média

---
+++ aula003/atividade001/app.js
@@ -0,0 +1,12 @@
+let n1 = 0, n2 = 0, n3 = 0, n4 = 0, media = 0
+n1 = 7
+n2 = 5.4
+n3 = 3.23
+n4 = 8
+media = (n1 + n2 + n3 + n4) / 4
+
+//console.log("média = " + media.toFixed)
+
+// 1 - como fazer para o resultado apresentar apenas 2 casas decimais
+console.log("média = " + media.toFixed(2))
+// 2 - como fazer para o programa solicitar as notas para o usuário
---
+++ aula003/atividade002/app.js
@@ -0,0 +1,4 @@
+function fnConverterValor (){
+    console.log("foi convertido")
+}
+
