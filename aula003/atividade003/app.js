4349708bacd5061442c0e5bb299a83c8ec96411b
Kaio <72668772+AkaliSmoke@users.noreply.github.com>
2025-11-10 22:25:29 -0300

    atividade003
    Salvando

---
+++ aula003/atividade003/app.js
@@ -0,0 +1,24 @@
+//function fnJogo(){
+//    let chute = 0, numeroMagico = 3
+//    chute = document.getElementById("valor").value
+//
+//    if(chute == numeroMagico){
+//       document.getElementById("resultado").innerText = "Acertou 😊"
+//    }else{
+//        document.getElementById("resultado").innerText = "Errou 😞"
+//    }
+//
+//}
+
+// 1 - como fazer um sorteio de número de 1 a 10 ao invés de deixar fixo o 3
+
+function fnJogo(){
+    let chute = 0, numeroMagico = 3
+    chute = document.getElementById("valor").value
+
+    if(chute == numeroMagico){
+       document.getElementById("resultado").innerText = "Acertou 😊"
+    }else{
+        document.getElementById("resultado").innerText = "Errou 😞"
+    }
+}
